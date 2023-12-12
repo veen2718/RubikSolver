567672acf26e2dfabd2858159265c4f26fa7e941
praveen <praveenmohadevan@gmail.com>
2023-12-11 20:47:32 -0800

    added layer objects

--- cube.py
+++ cube.py
@@ -2,6 +2,15 @@ from display import *
 
 import numpy as np
 
+
+def findIndex(arr,a,b):
+    try:
+        return arr.index(a)
+    except:
+        return arr.index(b)
+
+directions = ['T','L','F','R','B','D']
+
 touchingFaces = { #This dictionary contains information about all the faces touching a specific face. For example, the green face would correspond to [0,1,3,5] which are the indexes for white, orange, red and yellow
     
     # To Rotate a side, we take the three bottom squares on the face top of the face we are rotating and make that the leftmost column of the face to the right. Take the leftmost column of the face to the right, and make that the top squares on the face to the bottom, etc.
@@ -18,13 +27,77 @@ touchingFaces = { #This dictionary contains information about all the faces touc
 
 class corner:
     def __init__(self,contents):
-        self.contents = list(contents)
-    
+        self.contents = list(contents)#Will be a list like ["W",0,"Green","Red",0,0]
+        self.directions = []
+        for direction, value in zip(directions,self.contents):
+            setattr(self,direction,value)
+            if value != '0':
+                self.directions.append(direction)
+        
+        #WYdirection  = directions[findIndex(self.contents),"W","Y"]
+        
+class edge:
+    def __init__(self,color,direction):
+            self.c1 = list(color)[0]
+            self.c2 = list(color)[1]
+            self.d1 = list(direction)[0]
+            self.d2 = list(direction)[1]
+
+class center:
+    def __init__(self,color, direction):
+        self.color = color
+        self.direction = direction
+
+
+class layer:
+    def __init__(self, edges,corners=None):
+        if corners:
+            self.corners = [corner(x) for x in corners]
+        else:
+            self.corners = None
+        self.edges = [edge(x[0],x[1]) for x in edges]
 
 
 
 class cube:
     def __init__(self):
+        Corners = [
+            "WOG000",
+            "W0GR00",
+            "W00RB0",
+            "WO00B0",
+
+            "0OG00Y",
+            "00GR0Y",
+            "000RBY",
+            "0O00BY",
+        ]
+
+        Edges = [
+            ["WO","TL"],
+            ["WG","TF"],
+            ["WR","TR"],
+            ["WB","TB"],
+
+            ["OG","LF"],
+            ["GR","FR"],
+            ["RB","RB"],
+            ["BO","BL"],
+
+            ["YO","BL"],
+            ["YG","BF"],
+            ["YR","BR"],
+            ["YB","BB"],
+        ]
+
+        self.layers = [
+            layer(Edges[0:4],Corners[0:4]),
+            layer(Edges[4:8]),
+            layer(Edges[8:12],Corners[4:8])
+        ]
+        
+
+
         self.contents = [
             [["W" for _ in range(3)] for _ in range(3)],
             [["O" for _ in range(3)] for _ in range(3)],
@@ -57,3 +130,4 @@ class cube:
         for i in range(4):
             drawFace(squareSize*i*3 + squareGap*(i+1), squareGap*2 + squareSize*3, self.contents[i+1], canvas)
         drawFace(squareSize*3 + 2*squareGap, 2*squareSize*3 + 3*squareGap, self.contents[5],canvas)
+
